0bdd61f5edddcea3f0b623d5b2684b847395d464
Kyoungsu Park <kyopark2014@gmail.com>
2024-11-27 14:53:30 +0900

    update

--- cdk-monitoring/lib/cdk-monitoring-stack.ts
+++ cdk-monitoring/lib/cdk-monitoring-stack.ts
@@ -1,21 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-import * as genai from '@cdklabs/generative-ai-cdk-constructs';
 
 import { BedrockCwDashboard } from '@cdklabs/generative-ai-cdk-constructs';
-import { Stack, StackProps, Aws } from 'aws-cdk-lib';
 
 export class CdkMonitoringStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkMonitoringQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
 
     const bddashboard = new BedrockCwDashboard(this, 'BedrockDashboardConstruct', {});
 
